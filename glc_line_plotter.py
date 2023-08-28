2c29e3b6ae31fa7d8a0adbf752630386d3dab32b
AvaAvarai <alice.williams.tech@gmail.com>
2023-08-27 19:12:59 -0700

    Plot LDA boundary, zorder, pop endpoints

--- glc_line_plotter.py
+++ glc_line_plotter.py
@@ -53,6 +53,42 @@ def get_lda_coefficients(df, feature_columns, label_column):
     lda.fit(X, y)
     return lda.coef_[0]
 
+# Add this function to calculate the final x-coordinate of the GLC-L glyph
+def calculate_final_x(row, angles):
+    x_prev = 0
+    for i, feature in enumerate(row.index):
+        a_i = row[i]  # Length of line segment
+        theta_i = angles[i]  # Angle based on coefficient
+        x_i = x_prev + a_i * np.cos(theta_i)
+        x_prev = x_i
+    return x_i
+
+# Modify this function to use calculate_final_x
+def find_lda_separation_line(df, lda_model, feature_columns, label_column, angles):
+    # Get the actual and predicted labels
+    X = df[feature_columns].values
+    y = df[label_column].values
+    y_pred = lda_model.predict(X)
+
+    # Identify the misclassified points
+    misclassified = (y != y_pred)
+    misclassified_df = df[misclassified]
+
+    # Get the x-projections of the misclassified points
+    x_projections = misclassified_df[feature_columns].apply(lambda row: calculate_final_x(row, angles), axis=1)
+    
+    # Find the left-most and right-most misclassified points
+    leftmost_x = x_projections.min()
+    rightmost_x = x_projections.max()
+
+    # Calculate the midpoint between the left-most and right-most misclassified points
+    midpoint_x = (leftmost_x + rightmost_x) / 2
+    print(midpoint_x)
+    return midpoint_x
+
+def plot_lda_separation_line(midpoint_x):
+    plt.axvline(x=midpoint_x, color='orange', linestyle='--', linewidth=1)
+
 def plot_glyphs(df, dataset_name, coefficients=None):
     feature_columns = [col for col in df.columns if col != 'class']
     label_column = 'class'
@@ -66,54 +102,63 @@ def plot_glyphs(df, dataset_name, coefficients=None):
     unique_labels = df[label_column].unique()
     colors = plt.cm.jet(np.linspace(0, 1, len(unique_labels)))
     label_to_color = dict(zip(unique_labels, colors))
-    
+    # Fit the LDA model
+    lda_model = LinearDiscriminantAnalysis()
+    X = df[feature_columns].values
+    y = df[label_column].values
+    lda_model.fit(X, y)
     plt.figure(figsize=(10, 20))
     
     max_x_value = 0
-    for index, row in df.iterrows():
-        x_prev = 0
-        for i, feature in enumerate(feature_columns):
-            a_i = row[feature]  # Length of line segment
-            theta_i = angles[i]  # Angle based on coefficient
-            x_i = x_prev + a_i * np.cos(theta_i)
-            x_prev = x_i
-        max_x_value = max(max_x_value, x_i)
+    for unique_label in unique_labels:
+        for index, row in df[df[label_column] == unique_label].iterrows():
+            x_prev = 0
+            for i, feature in enumerate(feature_columns):
+                a_i = row[feature]
+                theta_i = angles[i]
+                x_i = x_prev + a_i * np.cos(theta_i)
+                x_prev = x_i
+            max_x_value = max(max_x_value, x_i)
     
     plt.subplot(2, 1, 1)
     first_class = unique_labels[0]
     for index, row in df[df[label_column] == first_class].iterrows():
         x_prev, y_prev = 0, 0
-        for i, feature in enumerate(feature_columns):  # Added 'i' to enumerate
+        for i, feature in enumerate(feature_columns):
             a_i = row[feature]
             theta_i = angles[i]
             x_i = x_prev + a_i * np.cos(theta_i)
             y_i = y_prev + a_i * np.sin(theta_i)
-            plt.plot([x_prev, x_i], [y_prev, y_i], color=label_to_color[row[label_column]], alpha=0.33)
+            plt.plot([x_prev, x_i], [y_prev, y_i], color=label_to_color[row[label_column]], alpha=0.33, zorder=1)
             x_prev, y_prev = x_i, y_i
         plt.scatter(x_i, 0, marker='|', color=label_to_color[row[label_column]], s=100)
-        endpoint_color = lighten_color(label_to_color[row[label_column]], 0.3)  # Lighten the color
-        plt.scatter(x_i, y_i, color=endpoint_color, s=30)
-    
+        plt.scatter(x_i, y_i, marker='s', color='black', s=10, zorder=2)
+    # Find the midpoint for the LDA separation line and plot it
+    midpoint_x = find_lda_separation_line(df, lda_model, feature_columns, label_column, angles)
+    plot_lda_separation_line(midpoint_x)
     plt.xlim(0, max_x_value + 0.1)
     plt.title(f'GLC-L Graph of {dataset_name} - Class: {first_class}')
     
     plt.subplot(2, 1, 2)
-    for index, row in df[df[label_column] != first_class].iterrows():
-        x_prev, y_prev = 0, 0
-        for i, feature in enumerate(feature_columns):  # Added 'i' to enumerate
-            a_i = row[feature]
-            theta_i = angles[i]
-            x_i = x_prev + a_i * np.cos(theta_i)
-            y_i = y_prev + a_i * np.sin(theta_i)
-            plt.plot([x_prev, x_i], [y_prev, y_i], color=label_to_color[row[label_column]], alpha=0.33)
-            x_prev, y_prev = x_i, y_i
-        plt.scatter(x_i, 0, marker='|', color=label_to_color[row[label_column]], s=100)
-        endpoint_color = lighten_color(label_to_color[row[label_column]], 0.3)  # Lighten the color
-        plt.scatter(x_i, y_i, color=endpoint_color, s=30)
-        
+    for other_class in unique_labels[1:]:
+        for index, row in df[df[label_column] == other_class].iterrows():
+            x_prev, y_prev = 0, 0
+            for i, feature in enumerate(feature_columns):  # Added 'i' to enumerate
+                a_i = row[feature]
+                theta_i = angles[i]
+                x_i = x_prev + a_i * np.cos(theta_i)
+                y_i = y_prev + a_i * np.sin(theta_i)
+                plt.plot([x_prev, x_i], [y_prev, y_i], color=label_to_color[row[label_column]], alpha=0.33, zorder=1)
+                x_prev, y_prev = x_i, y_i
+            plt.scatter(x_i, 0, marker='|', color=label_to_color[row[label_column]], s=100)
+            plt.scatter(x_i, y_i, marker='s', color='black', s=10, zorder=2)
+    # Find the midpoint for the LDA separation line and plot it
+    midpoint_x = find_lda_separation_line(df, lda_model, feature_columns, label_column, angles)
+    plot_lda_separation_line(midpoint_x)
     plt.xlim(0, max_x_value + 0.1)
     plt.gca().invert_yaxis()
-    plt.title(f'GLC-L Graph of {dataset_name} - Additional Classes')
+    second_class = unique_labels[1]
+    plt.title(f'GLC-L Graph of {dataset_name} - {second_class}')
     
     custom_lines = [plt.Line2D([0], [0], color=color, lw=4) for color in label_to_color.values()]
     plt.legend(custom_lines, unique_labels, title='Class')
